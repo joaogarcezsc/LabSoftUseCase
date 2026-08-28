0fbe97a19716db9d19eadabed2b2bf7da329988a
joaomanoell <joaomanoell875@outlook.com.br>
2026-08-28 09:42:12 -0300

    projetotaskzero

---
+++ SLNTaskZero/AppTaskZero/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AppTaskZero.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
